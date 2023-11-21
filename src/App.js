b97862cdf2abc71f0b94402caa7f081fd89e3634
Yashaswi <gupta.yashaswi15@gmail.com>
2023-11-22 02:14:36 +0530

    changed css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState } from 'react';
 import Task from './Task';
-import './App.css';
+import './styles.css';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -49,17 +49,25 @@ const App = () => {
   };
 
   return (
-    <div className="container">
-      <h1>Task Manager</h1>
-      <div>
+    <div className="container bg-gray-100 p-4 ">
+      <h1 className="text-3xl font-bold text-center text-purple-600">
+        Task Manager
+      </h1>
+      <div className="flex justify-center mt-4">
         <input
           type="text"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
+          className="border border-purple-300 rounded px-2 py-1 mr-2"
         />
-        <button onClick={addTask}>Add Task</button>
+        <button
+          onClick={addTask}
+          className="bg-purple-600 text-white rounded px-4 py-2"
+        >
+          Add Task
+        </button>
       </div>
-      <div className="task-container">
+      <div className="mt-8 grid grid-cols-3 gap-4">
         {tasks.map((task) => (
           <Task
             key={task.id}
@@ -73,13 +81,19 @@ const App = () => {
         ))}
       </div>
       {editingTaskId !== null && (
-        <div>
+        <div className="flex justify-center mt-4">
           <input
             type="text"
             value={editingTaskText}
             onChange={(e) => setEditingTaskText(e.target.value)}
+            className="border border-purple-300 rounded px-2 py-1 mr-2"
           />
-          <button onClick={updateTask}>Update Task</button>
+          <button
+            onClick={updateTask}
+            className="bg-purple-600 text-white rounded px-4 py-2"
+          >
+            Update Task
+          </button>
         </div>
       )}
     </div>
